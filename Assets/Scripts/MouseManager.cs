a105aba0d99e4f9004870925e5900348e729cedc
Program-Jim <1072118335@qq.com>
2024-12-21 19:56:47 +0800

    Initial commit

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Rendering.Universal;
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3>{}
+
+/// <summary>
+/// Manage the input controll of PC's mouse. 
+/// </summary>
+public class MouseManager : MonoBehaviour
+{
+    public EventVector3 OnMouseClicked;
+    public Color color;
+    
+    private Ray ray;
+    private RaycastHit hitInfo;
+
+    private void Awake()
+    {
+        Debug.Log("Mouse Manager is on.");
+    }
+
+    private void Update()
+    {
+        SetCursorTexture();
+        MouseControl();
+    }
+
+    /// <summary>
+    /// A function which set the texture of PC's cursor.
+    /// </summary>
+    void SetCursorTexture()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            //TODO: Change cursor's texture
+            
+        }
+    }
+
+    /// <summary>
+    /// Manage the input controll of PC's mouse.
+    /// </summary>
+    void MouseControl()
+    {
+        if (Input.GetMouseButtonDown(0) && hitInfo.collider != null)
+        {
+            if (hitInfo.collider.gameObject.CompareTag("Ground"))
+            {
+                OnMouseClicked?.Invoke(hitInfo.point);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TheKnight-RPG-URP-3D
+ My first 3D RPG Game made with Unity.
