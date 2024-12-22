1679a28c8901d3da4d86992d8cc244f2fdf84fc2
Program-Jim <1072118335@qq.com>
2024-12-22 12:30:43 +0800

    Update

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 [System.Serializable]
