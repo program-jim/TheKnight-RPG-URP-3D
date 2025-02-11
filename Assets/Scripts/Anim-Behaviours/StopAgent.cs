4cc97720e95d4b2845cc6771c17cec2830112895
Program-Jim <1072118335@qq.com>
2025-02-11 13:37:19 +0800

    Update scripts and animator.

--- Assets/Scripts/Anim-Behaviours/StopAgent.cs
+++ Assets/Scripts/Anim-Behaviours/StopAgent.cs
@@ -8,19 +8,19 @@ public class StopAgent : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        animator.GetComponent<NavMeshAgent>().isStopped = true;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<NavMeshAgent>().isStopped = true;
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        animator.GetComponent<NavMeshAgent>().isStopped = false;
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
