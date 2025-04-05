5e84d3e6ef4e6fe6cd9b17b46ae74dbee212456f
Program-Jim <1072118335@qq.com>
2025-04-05 16:57:16 +0800

    Add health bar features.

---
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarUI : MonoBehaviour
+{
+    public GameObject healthUIPrefab;
+    public Transform barPoint;
+
+    private Image healthSlider;
+    private Transform UIbar;
+    private Transform camPoint;
+
+
+
+}
