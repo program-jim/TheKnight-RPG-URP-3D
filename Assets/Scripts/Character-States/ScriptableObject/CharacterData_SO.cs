493e6b78f05fcf81a64c7b8c3fe0b521c8f800fe
Program-Jim <1072118335@qq.com>
2025-04-06 12:40:08 +0800

    Update CharacterData_SO.cs
    Add level up features.

--- Assets/Scripts/Character-States/ScriptableObject/CharacterData_SO.cs
+++ Assets/Scripts/Character-States/ScriptableObject/CharacterData_SO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,4 +14,47 @@ public class CharacterData_SO : ScriptableObject
     public int currentHealth;
     public int baseDefence;
     public int currentDefence;
+
+    [Header("Kill")]
+    public int killPoint;
+
+    [Header("Level")]
+    public int currentLevel;
+    public int maxLevel;
+    public int baseExp;
+    public int currentExp;
+    public float levelBuff;
+
+    public float LevelMultiplier
+    {
+        get
+        {
+            return 1 + (currentLevel - 1) * levelBuff;
+        }
+    }
+
+    public void UpdateExp(int point)
+    {
+        currentExp += point;
+
+        if (currentExp >= baseExp)
+        {
+            LevelUp();
+        }
+    }
+
+    private void LevelUp()
+    {
+        // All methods you want to update your data.
+
+        // Update Level
+        currentLevel = Mathf.Clamp(currentLevel + 1, 0, maxLevel);
+        baseExp += (int)(baseExp * LevelMultiplier);
+
+        // Update health
+        maxHealth = (int)(maxHealth * LevelMultiplier);
+        currentHealth = maxHealth;
+
+        Debug.Log("LEVEL UP !!! " + "Current Level: " + currentLevel + " Max health: " + maxHealth);
+    }
 }
