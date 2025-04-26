c14015ad4746b19321834043baded10462e5ca56
Program-Jim <1072118335@qq.com>
2025-04-26 18:56:20 +0800

    Add portal features

---
+++ Assets/Scripts/Transition/TransitionPoint.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionPoint : MonoBehaviour
+{
+    [Header("Transition Info")]
+    public string sceneName;
+    public TransitionType transitionType;
+
+
+}
+
+public enum TransitionType
+{
+    SameScene, DifferentScene
+}
