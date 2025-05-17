a344fe23b856b09284efa7f60a1867233bbd9030
Program-Jim <1072118335@qq.com>
2025-05-17 12:57:43 +0800

    Update contents.

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -14,5 +14,32 @@ public class MainMenu : MonoBehaviour
         newGameButton = transform.GetChild(1).GetComponent<Button>();
         continueGameButton = transform.GetChild(2).GetComponent<Button>();
         quitGameButton = transform.GetChild(3).GetComponent<Button>();
+
+        newGameButton.onClick.AddListener(NewGame);
+        continueGameButton.onClick.AddListener(ContinueGame);
+        quitGameButton.onClick.AddListener(QuitGame);
+    }
+
+    public void NewGame()
+    {
+        // Delete All PlayerPrefs data
+        PlayerPrefs.DeleteAll();
+        Debug.Log("Deleted All PlayerPrefs data !!!");
+
+        // Scene switching
+
+
+    }
+
+    public void ContinueGame()
+    {
+        // Scene switching and read PlayerPrefs data.
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
     }
 }
