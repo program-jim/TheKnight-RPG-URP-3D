c586d391453858962c882fe5b00b945614b536e4
Program-Jim <1072118335@qq.com>
2025-06-02 18:23:02 +0800

    Update MainMenu.cs
    Add some Timeline codes.

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Playables;
 
 public class MainMenu : MonoBehaviour
 {
@@ -9,23 +10,27 @@ public class MainMenu : MonoBehaviour
     Button continueGameButton;
     Button quitGameButton;
 
+    PlayableDirector director;
+
     private void Awake()
     {
         newGameButton = transform.GetChild(1).GetComponent<Button>();
         continueGameButton = transform.GetChild(2).GetComponent<Button>();
         quitGameButton = transform.GetChild(3).GetComponent<Button>();
 
-        newGameButton.onClick.AddListener(NewGame);
+        director = FindObjectOfType<PlayableDirector>();
+        director.stopped += NewGame;
+
+        newGameButton.onClick.AddListener(PlayTimeline);
         continueGameButton.onClick.AddListener(ContinueGame);
         quitGameButton.onClick.AddListener(QuitGame);
     }
 
-    public void NewGame()
+    public void NewGame(PlayableDirector director)
     {
         // Delete All PlayerPrefs data
         PlayerPrefs.DeleteAll();
         Debug.Log("Deleted All PlayerPrefs data !!!");
-
         // Scene switching
         Debug.Log("Transfering To First Level !!!");
         SceneController.Instance.TransitionToFirstLevel();
@@ -41,5 +46,10 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quit Game");
         Application.Quit();
+    }
+
+    public void PlayTimeline()
+    {
+        director.Play();
     }
 }
