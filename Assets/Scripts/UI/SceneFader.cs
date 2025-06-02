70de1f4b700460bdac6fd2b0d20dd1a683c9b979
Program-Jim <1072118335@qq.com>
2025-06-02 19:01:16 +0800

    Add fade logical content.

---
+++ Assets/Scripts/UI/SceneFader.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneFader : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    public float fadeInDuration;
+    public float fadeOutDuration;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public IEnumerator FadeOut(float time)
+    {
+        while (canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha += Time.deltaTime / time;
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeIn(float time)
+    {
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= Time.deltaTime / time;
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeOutIn()
+    {
+        yield return FadeOut(fadeOutDuration);
+        yield return FadeIn(fadeInDuration);
+    }
+
+    public IEnumerator FadeInOut()
+    {
+        yield return FadeIn(fadeInDuration);
+        yield return FadeOut(fadeOutDuration);
+    }
+}
